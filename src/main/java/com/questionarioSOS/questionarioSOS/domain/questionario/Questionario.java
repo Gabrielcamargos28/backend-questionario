3a666e90b4772cbd6c1e925763b0b62d7fe33017
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-07-08 00:03:27 -0300

    feat(questionario): adicionado entidade e controller para questionario

---
+++ src/main/java/com/questionarioSOS/questionarioSOS/domain/questionario/Questionario.java
@@ -0,0 +1,32 @@
+package com.questionarioSOS.questionarioSOS.domain.questionario;
+
+import com.questionarioSOS.questionarioSOS.domain.questao.Questao;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_questionario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Questionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @OneToMany(mappedBy = "tb_questionario")
+    private Set<Questao> questoes;
+    private Boolean finalizado;
+
+
+    public Questionario(DadosIniciarFormulario dados) {
+        this.nome = dados.nome();
+    }
+}
---
+++ src/main/java/com/questionarioSOS/questionarioSOS/domain/questionario/QuestionarioRepository.java
@@ -0,0 +1,6 @@
+package com.questionarioSOS.questionarioSOS.domain.questionario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionarioRepository extends JpaRepository<Questionario, Long> {
+}
